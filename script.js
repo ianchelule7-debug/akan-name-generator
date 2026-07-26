fa0206d47e4c5bd3762e44332460a95a55ff470b
Ian <Ianchelule7@gmail.com>
2026-07-26 19:38:47 +0300

    Retrieve user input from form

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ const form = document.getElementById("akanForm");
 form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    console.log("Form submitted!");
+    const birthDate = document.getElementById("birthDate").value;
+
+const gender = document.getElementById("gender").value;
+
+console.log(birthDate);
+
+console.log(gender);
+
 });
